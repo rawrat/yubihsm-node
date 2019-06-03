d83278f3af4667da66e8132beb08aef90fd5b84f
Angelo Laub <angelolaub@gmail.com>
2019-06-03 21:30:36 +0200

    exports

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
 var addon = require('bindings')('addon.node')
-
-class Yubi {
-  constructor(config) {
-    this.ctx = addon.get_context(config)
-    // this.keymap = 
-  }
-  
-  
-}
+module.exports = addon.Session
